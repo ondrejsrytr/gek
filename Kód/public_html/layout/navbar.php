1369dbfb4c89f6550e0e3279933a31cf0fc7e6f9
Ondřej Šrytr <ondrejsrytr@ondrejsrytr.cz>
2020-11-11 13:49:24 +0100

    Úprava hlavičky stránky

--- Kód/public_html/layout/navbar.php
+++ Kód/public_html/layout/navbar.php
@@ -22,17 +22,21 @@ include_once ROOT . "session.php";
                     <?php
                     if ($_SESSION['user'] != null) {
                         ?>
-                        <div id="profile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="cursor: pointer;">
-                            <?= $_SESSION['user']->getJmeno() ?>
-                        </div>
-                        <div class="dropdown-menu" aria-labelledby="profile">
-                            <h6 class="dropdown-header">Profil</h6>
-                            <a class="dropdown-item" type="button" href="">Příspěvky</a>
-                            <a class="dropdown-item" type="button" href="/edit-profile">Správa účtu</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" type="button" href="/helpdesk">Helpdesk</a>
-                            <a class="dropdown-item" type="button" href="/logout">Odhlásit se</a>
-                        </div>
+                        <li class="nav-item">
+                            <div class="nav-link dropdown">
+                                <div id="profile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="cursor: pointer;">
+                                    <?= $_SESSION['user']->getJmeno() ?>
+                                </div>
+                                <div class="dropdown-menu" aria-labelledby="profile">
+                                    <h6 class="dropdown-header">Profil</h6>
+                                    <a class="dropdown-item" type="button" href="">Příspěvky</a>
+                                    <a class="dropdown-item" type="button" href="/edit-profile">Správa účtu</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" type="button" href="/helpdesk">Helpdesk</a>
+                                    <a class="dropdown-item" type="button" href="/logout">Odhlásit se</a>
+                                </div>
+                            </div>
+                        </li>
                         <?php
                     }
                     else {
