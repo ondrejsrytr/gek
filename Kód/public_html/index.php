4b306e8ba07da0306e3ad6354325daa1fad271ef
Smazenka <72349923+Smazenka@users.noreply.github.com>
2020-11-13 15:40:34 +0100

    Vypsání článků
    Přidána datatable s článkama

--- Kód/public_html/index.php
+++ Kód/public_html/index.php
@@ -11,6 +11,14 @@ include ROOT . "session.php";
     <?php
     include ROOT . "layout/head.php";
     ?>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js">
+    </script>
+    <script>
+        $(document).ready( function () {
+    $('#main_table').DataTable();
+    });
+    </script>
 </head>
 
 <body>
@@ -20,7 +28,61 @@ include ROOT . "session.php";
 
 
 <main>
+<?php //Vypsání datatables
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL);
 
+define('SQL_HOST', 'sql4.webzdarma.cz');
+define('SQL_DBNAME', 'gekwzcz3751');
+define('SQL_USERNAME', 'gekwzcz3751');
+define('SQL_PASSWORD', '&976l3lW9b^12.8J)ykv');
+
+$dsn = 'mysql:dbname=' . SQL_DBNAME . ';host=' . SQL_HOST . '';
+$user = SQL_USERNAME;
+$password = SQL_PASSWORD;
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Připojení k databázi selhalo: ' . $e->getMessage());
+}
+?>
+<table id="main_table">
+<thead>
+<th>Název článku</th>
+<th>Autor</th>
+<th>Datum vydání</th>
+<th>Odkaz ke stažení</th>
+</thead>
+<tbody>
+<?php 
+    //SELECT Users.jmeno, Clanky.nazev, Clanky.datum_vydani FROM Clanky INNER JOIN Users on Clanky.autor = Users.id
+    $dotaz = "SELECT Users.jmeno, Clanky.nazev, Clanky.datum_vydani FROM Clanky INNER JOIN Users on Clanky.autor = Users.id";
+    $vysledek = $pdo->prepare($dotaz);
+    $vysledek->execute();
+    $result = $vysledek->fetchAll(\PDO::FETCH_ASSOC);
+    $pocet = $vysledek->rowCount();
+    for($i = 0; $i < $pocet; $i++){
+    echo '<tr>';
+        echo '<td>';
+            print $result[$i]["nazev"];
+        echo '</td>';
+        echo '<td>';
+            print $result[$i]["jmeno"];
+        echo '</td>';
+        echo '<td>';
+            print $result[$i]["datum_vydani"];
+        echo '</td>';
+        echo '<td>';
+            echo "Odkaz zde"; //TODO
+        echo '</td>';
+    echo '</tr>';
+    }
+?>
+</tbody>
+</table>
 </main>
 
     <?php
