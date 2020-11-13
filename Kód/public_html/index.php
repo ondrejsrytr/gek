a2d0d370a5626ba84af7cda07d3166b443d60d32
Ondřej Šrytr <ondrejsrytr@ondrejsrytr.cz>
2020-11-13 16:29:43 +0100

    Změna vzhledu vypisovaných příspěvků

--- Kód/public_html/index.php
+++ Kód/public_html/index.php
@@ -6,88 +6,89 @@ include ROOT . "session.php";
 <!doctype html>
 <html lang="cs">
 
-<head>
-    <title>Hlavní stránka</title>
-    <?php
-    include ROOT . "layout/head.php";
-    ?>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js">
-    </script>
-    <script>
-        $(document).ready( function () {
-    $('#main_table').DataTable();
-    });
-    </script>
-</head>
+    <head>
+        <title>Hlavní stránka</title>
+        <?php
+        include ROOT . "layout/head.php";
+        ?>
+    </head>
 
-<body>
+    <body>
     <?php
     include ROOT . "layout/navbar.php";
     ?>
 
 
-<main>
-<?php //Vypsání datatables
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL);
+    <main class="container d-flex h-100 flex-column pt-5">
+        <div id="article_filter" class="btn-group btn-group-toggle" data-toggle="buttons">
+            <label class="btn btn-secondary active">
+                <input type="radio" name="options" id="option1" checked> Články
+            </label>
+            <label class="btn btn-secondary">
+                <input type="radio" name="options" id="option2"> Časopisy
+            </label>
+        </div>
+        <br>
+        <?php //Vypsání datatables
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
 
-define('SQL_HOST', 'sql4.webzdarma.cz');
-define('SQL_DBNAME', 'gekwzcz3751');
-define('SQL_USERNAME', 'gekwzcz3751');
-define('SQL_PASSWORD', '&976l3lW9b^12.8J)ykv');
+        define('SQL_HOST', 'sql4.webzdarma.cz');
+        define('SQL_DBNAME', 'gekwzcz3751');
+        define('SQL_USERNAME', 'gekwzcz3751');
+        define('SQL_PASSWORD', '&976l3lW9b^12.8J)ykv');
 
-$dsn = 'mysql:dbname=' . SQL_DBNAME . ';host=' . SQL_HOST . '';
-$user = SQL_USERNAME;
-$password = SQL_PASSWORD;
+        $dsn = 'mysql:dbname=' . SQL_DBNAME . ';host=' . SQL_HOST . '';
+        $user = SQL_USERNAME;
+        $password = SQL_PASSWORD;
 
-try {
-    $pdo = new PDO($dsn, $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die('Připojení k databázi selhalo: ' . $e->getMessage());
-}
-?>
-<table id="main_table">
-<thead>
-<th>Název článku</th>
-<th>Autor</th>
-<th>Datum vydání</th>
-<th>Odkaz ke stažení</th>
-</thead>
-<tbody>
-<?php 
-    //SELECT Users.jmeno, Clanky.nazev, Clanky.datum_vydani FROM Clanky INNER JOIN Users on Clanky.autor = Users.id
-    $dotaz = "SELECT Users.jmeno, Clanky.nazev, Clanky.datum_vydani FROM Clanky INNER JOIN Users on Clanky.autor = Users.id";
-    $vysledek = $pdo->prepare($dotaz);
-    $vysledek->execute();
-    $result = $vysledek->fetchAll(\PDO::FETCH_ASSOC);
-    $pocet = $vysledek->rowCount();
-    for($i = 0; $i < $pocet; $i++){
-    echo '<tr>';
-        echo '<td>';
-            print $result[$i]["nazev"];
-        echo '</td>';
-        echo '<td>';
-            print $result[$i]["jmeno"];
-        echo '</td>';
-        echo '<td>';
-            print $result[$i]["datum_vydani"];
-        echo '</td>';
-        echo '<td>';
-            echo "Odkaz zde"; //TODO
-        echo '</td>';
-    echo '</tr>';
-    }
-?>
-</tbody>
-</table>
-</main>
+        try {
+            $pdo = new PDO($dsn, $user, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('Připojení k databázi selhalo: ' . $e->getMessage());
+        }
+        ?>
+        <table id="main_table" class="table table-striped table-bordered">
+            <thead>
+            <th>Název článku</th>
+            <th>Autor</th>
+            <th>Datum vydání</th>
+            <th>Odkaz ke stažení</th>
+            </thead>
+            <tbody>
+            <?php
+            //SELECT Users.jmeno, Clanky.nazev, Clanky.datum_vydani FROM Clanky INNER JOIN Users on Clanky.autor = Users.id
+            $dotaz = "SELECT Users.jmeno, Clanky.nazev, Clanky.datum_vydani FROM Clanky INNER JOIN Users on Clanky.autor = Users.id";
+            $vysledek = $pdo->prepare($dotaz);
+            $vysledek->execute();
+            $result = $vysledek->fetchAll(\PDO::FETCH_ASSOC);
+            $pocet = $vysledek->rowCount();
+            for ($i = 0; $i < $pocet; $i++) {
+                echo '<tr>';
+                echo '<td>';
+                print $result[$i]["nazev"];
+                echo '</td>';
+                echo '<td>';
+                print $result[$i]["jmeno"];
+                echo '</td>';
+                echo '<td>';
+                print $result[$i]["datum_vydani"];
+                echo '</td>';
+                echo '<td>';
+                echo "Odkaz zde"; //TODO
+                echo '</td>';
+                echo '</tr>';
+            }
+            ?>
+            </tbody>
+        </table>
+    </main>
 
     <?php
     include ROOT . "layout/footer.php";
     ?>
-</body>
+    </body>
 
 </html>
--- Kód/public_html/layout/head.php
+++ Kód/public_html/layout/head.php
@@ -4,4 +4,17 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+<!--
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
+        -->
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+<script>
+    $(document).ready(function () {
+        $('#main_table').DataTable();
+    });
+</script>
 <link rel="stylesheet" href="/layout/css/main.css">
