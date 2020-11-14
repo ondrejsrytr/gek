d42d83ee95c6167be2497790bfc4c59581654a9a
Ondřej Šrytr <ondrejsrytr@ondrejsrytr.cz>
2020-11-14 23:29:07 +0100

    Stránka pro kontaktování administrátorů

---
+++ Kód/public_html/support/index.php
@@ -0,0 +1,35 @@
+<?php
+    define('ROOT', "/3w/users/g/gek.wz.cz/web/");
+    include_once ROOT."classes/User.php";
+    include ROOT."session.php";
+
+    if(!isset($_SESSION['user'])) {
+        $_SESSION['referer'] = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        header("Location: /login");
+    }
+    ?>
+    <!doctype html>
+    <html lang="cs">
+
+    <head>
+        <title>Podpora</title>
+        <?php
+        include ROOT . "layout/head.php";
+        ?>
+    </head>
+
+    <body>
+        <?php
+        include ROOT . "layout/navbar.php";
+        ?>
+
+    <main>
+
+    </main>
+
+        <?php
+        include ROOT . "layout/footer.php";
+        ?>
+    </body>
+
+    </html>
