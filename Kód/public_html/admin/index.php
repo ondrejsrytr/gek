f82400655afa92d55f78d502bfc1aa2fc056c004
Ondřej Šrytr <ondrejsrytr@ondrejsrytr.cz>
2020-11-23 22:25:28 +0100

    Soubor indexu v administraci

---
+++ Kód/public_html/admin/index.php
@@ -0,0 +1,23 @@
+<?php
+include "head.php";
+?>
+    <div class="container admin">
+        <div class="row row-eq-height">
+            <div class="col-lg-3">
+                <div class="col-menu">
+                    <ul class="nav nav-pills flex-column">
+                        <?php include "menu.php"; ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-9">
+                <div class="col-content">
+                    <!-- OBSAH STRÁNKY -->
+                    <h4 class="my-0">Výchozí stránka</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+include "foot.php";
+?>
