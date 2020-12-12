7584c456593025799f6355a85421bd17f342544b
Smazenka <72349923+Smazenka@users.noreply.github.com>
2020-12-12 14:02:32 +0100

    Funkce pro redaktora

---
+++ Kód/public_html/admin/article-submit/smazat.php
@@ -0,0 +1,14 @@
+<?php 
+define('ROOT', "/3w/users/g/gek.wz.cz/web/");
+include_once ROOT."classes/db.php";
+include_once ROOT."classes/ActivityLog.php";
+
+$dotaz = "DELETE FROM Clanky WHERE id = ?";
+$vysledek = $pdo->prepare($dotaz);
+$vysledek->execute(array($_POST["clanekid"]));
+
+header('Location: index.php');
+
+
+
+?>
