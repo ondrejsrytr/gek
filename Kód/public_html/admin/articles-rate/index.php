0d36ff6c22b987484beac2e5b38c059924cedcfa
Dmytro Radchuk <radchuk.d@gmail.com>
2020-12-27 21:10:25 +0100

    updated admin pages, added helpdesk page

--- Kód/public_html/admin/articles-rate/index.php
+++ Kód/public_html/admin/articles-rate/index.php
@@ -1,54 +1,54 @@
 <?php
-    include "../head.php";
+include "../head.php";
 ?>
-    <script type="text/javascript">
-        //todo: pojmenovat funkci inteligentneji
-        //pisu to ve dve rano, nechte me bejt
-        function hodTamTenText(nazev, id) {
-            document.getElementById("nazevCl").value = nazev;
-            document.getElementById("idCl").value = id;
-        }
-    </script>
-    <div class="container admin">
-        <div class="row row-eq-height">
-            <div class="col-lg-3">
-                <?php include "../menu.php"; ?>
-            </div>
-            <div class="col-lg-9">
-                <div class="col-content">
-                    <div class="d-flex justify-content-between align-items-baseline py-0">
-                        <h4 class="my-0">Články k ohodnocení</h4>
-                    </div>
-                    <?php //Vypsání datatables
-                    ini_set('display_errors', 1);
-                    ini_set('display_startup_errors', 1);
-                    error_reporting(E_ALL);
+<script type="text/javascript">
+    //todo: pojmenovat funkci inteligentneji
+    //pisu to ve dve rano, nechte me bejt
+    function hodTamTenText(nazev, id) {
+        document.getElementById("nazevCl").value = nazev;
+        document.getElementById("idCl").value = id;
+    }
+</script>
+<div class="container-fluid admin">
+    <div class="row row-eq-height">
+        <div class="col-lg-2">
+            <?php include "../menu.php"; ?>
+        </div>
+        <div class="col-lg-10">
+            <div class="col-content">
+                <div class="d-flex justify-content-between align-items-baseline py-0">
+                    <h4 class="my-0">Články k ohodnocení</h4>
+                </div>
+                <?php //Vypsání datatables
+                ini_set('display_errors', 1);
+                ini_set('display_startup_errors', 1);
+                error_reporting(E_ALL);
 
-                    define('SQL_HOST', 'sql4.webzdarma.cz');
-                    define('SQL_DBNAME', 'gekwzcz3751');
-                    define('SQL_USERNAME', 'gekwzcz3751');
-                    define('SQL_PASSWORD', '&976l3lW9b^12.8J)ykv');
+                define('SQL_HOST', 'sql4.webzdarma.cz');
+                define('SQL_DBNAME', 'gekwzcz3751');
+                define('SQL_USERNAME', 'gekwzcz3751');
+                define('SQL_PASSWORD', '&976l3lW9b^12.8J)ykv');
 
-                    $dsn = 'mysql:dbname=' . SQL_DBNAME . ';host=' . SQL_HOST . '';
-                    $user = SQL_USERNAME;
-                    $password = SQL_PASSWORD;
+                $dsn = 'mysql:dbname=' . SQL_DBNAME . ';host=' . SQL_HOST . '';
+                $user = SQL_USERNAME;
+                $password = SQL_PASSWORD;
 
-                    try {
-                        $pdo = new PDO($dsn, $user, $password);
-                        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    } catch (PDOException $e) {
-                        die('Připojení k databázi selhalo: ' . $e->getMessage());
-                    }
-                    ?>
-                    <div class="overflow-auto">
-                        <table id="main_table" class="table table-striped table-bordered overflow-auto">
-                            <thead>
+                try {
+                    $pdo = new PDO($dsn, $user, $password);
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch (PDOException $e) {
+                    die('Připojení k databázi selhalo: ' . $e->getMessage());
+                }
+                ?>
+                <div class="overflow-auto">
+                    <table id="main_table" class="table table-striped table-bordered overflow-auto">
+                        <thead>
                             <th>Název článku</th>
                             <th>Autor</th>
                             <th>Datum vydání</th>
                             <th>Odkaz ke stažení</th>
-                            </thead>
-                            <tbody>
+                        </thead>
+                        <tbody>
                             <?php
                             //SELECT Users.jmeno, Clanky.nazev, Clanky.datum_vydani FROM Clanky INNER JOIN Users on Clanky.autor = Users.id
                             $dotaz = "SELECT Users.id AS userid, Users.jmeno, Clanky.id, Clanky.nazev, Clanky.datum_vydani FROM Clanky INNER JOIN Users on Clanky.autor = Users.id WHERE (Clanky.stav = 0 AND Clanky.vybrany_r = ?)";
@@ -57,15 +57,15 @@
                             $result = $vysledek->fetchAll(\PDO::FETCH_ASSOC);
                             $pocet = $vysledek->rowCount();
                             for ($i = 0; $i < $pocet; $i++) {
-                                ?>
+                            ?>
                                 <tr>
                                     <td>
-                                        <?=$result[$i]["nazev"]?>
+                                        <?= $result[$i]["nazev"] ?>
                                     </td>
                                     <td>
                                         <?php
                                         print '
-                                            <div data-tooltip-id="'.$result[$i]["userid"].'" data-tooltip-name="'.$result[$i]["jmeno"].'" class="html-tooltip"><a href="/user?&id='.$result[$i]["id"].'">'.$result[$i]["jmeno"].'</a>
+                                            <div data-tooltip-id="' . $result[$i]["userid"] . '" data-tooltip-name="' . $result[$i]["jmeno"] . '" class="html-tooltip"><a href="/user?&id=' . $result[$i]["id"] . '">' . $result[$i]["jmeno"] . '</a>
                                                 <span class="tooltiptext">
                                                     <p>
                                                         <b>jmeno_uzivatele</b>
@@ -78,103 +78,102 @@
                                         ?>
                                     </td>
                                     <td>
-                                        <?=$result[$i]["datum_vydani"]?>
+                                        <?= $result[$i]["datum_vydani"] ?>
                                     </td>
                                     <td>
-                                        <a href="/upload/<?=$result[$i]["id"]?>.pdf" download="Clanek.pdf">Stáhnout</a>
+                                        <a href="/upload/<?= $result[$i]["id"] ?>.pdf" download="Clanek.pdf">Stáhnout</a>
                                     </td>
                                     <td>
-                                        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#hodnotitForm" onclick="hodTamTenText('<?=$result[$i]["nazev"]?>', '<?=$result[$i]["id"]?>')">Hodnotit</a>
+                                        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#hodnotitForm" onclick="hodTamTenText('<?= $result[$i]["nazev"] ?>', '<?= $result[$i]["id"] ?>')">Hodnotit</a>
                                     </td>
                                 </tr>
-                                <?php
+                            <?php
                             }
                             ?>
-                            </tbody>
-                        </table>
-                    </div>
+                        </tbody>
+                    </table>
                 </div>
-                <div class="modal" id="hodnotitForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                     aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                            <form action="../articles-all/rate.php" method="post" enctype="multipart/form-data">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Hodnocení příspěvku</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
+            </div>
+            <div class="modal" id="hodnotitForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <form action="../articles-all/rate.php" method="post" enctype="multipart/form-data">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Hodnocení příspěvku</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <label for="articleName">Název příspěvku</label>
+                                    <input type="text" class="form-control" name="articleName" id="nazevCl" disabled>
+                                    <input type="hidden" name="articleId" id="idCl">
                                 </div>
-                                <div class="modal-body">
-                                    <div class="form-group">
-                                        <label for="articleName">Název příspěvku</label>
-                                        <input type="text" class="form-control" name="articleName" id="nazevCl" disabled>
-                                        <input type="hidden" name="articleId" id="idCl">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <label for="aktualnost">Aktuálnost</label>
+                                        <select class="custom-select" name="aktualnost">
+                                            <option selected="selected">1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
                                     </div>
+                                    <div class="col-md-3">
+                                        <label for="originalita">Originalita</label>
+                                        <select class="custom-select" name="originalita">
+                                            <option selected="selected">1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <label for="odbornost">Odbornost</label>
+                                        <select class="custom-select" name="odbornost">
+                                            <option selected="selected">1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <label for="format">Formát</label>
+                                        <select class="custom-select" name="format">
+                                            <option selected="selected">1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
                                     <div class="row">
-                                        <div class="col-md-3">
-                                            <label for="aktualnost">Aktuálnost</label>
-                                            <select class="custom-select" name="aktualnost">
-                                                <option selected="selected">1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                            </select>
+                                        <div class="col-md-4">
+                                            <label for="articleApproved">Schváleno</label>
                                         </div>
-                                        <div class="col-md-3">
-                                            <label for="originalita">Originalita</label>
-                                            <select class="custom-select" name="originalita">
-                                                <option selected="selected">1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-md-3">
-                                            <label for="odbornost">Odbornost</label>
-                                            <select class="custom-select" name="odbornost">
-                                                <option selected="selected">1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-md-3">
-                                            <label for="format">Formát</label>
-                                            <select class="custom-select" name="format">
-                                                <option selected="selected">1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                <label for="articleApproved">Schváleno</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <input type="checkbox" class="form-control" name="articleApproved">
-                                            </div>
+                                        <div class="col-md-6">
+                                            <input type="checkbox" class="form-control" name="articleApproved">
                                         </div>
                                     </div>
                                 </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-light" data-dismiss="modal">Zavřít</button>
-                                    <button type="submit" class="btn btn-primary">Uložit</button>
-                                </div>
-                            </form>
-                        </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-light" data-dismiss="modal">Zavřít</button>
+                                <button type="submit" class="btn btn-primary">Uložit</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 <?php
 include "../foot.php";
 ?>
--- Kód/public_html/helpdesk/index.php
+++ Kód/public_html/helpdesk/index.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT', "/3w/users/g/gek.wz.cz/web/");
-include_once ROOT."classes/User.php";
-include ROOT."session.php";
+include_once ROOT . "classes/User.php";
+include ROOT . "session.php";
 
-if(!isset($_SESSION['user'])) {
+if (!isset($_SESSION['user'])) {
     $_SESSION['referer'] = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header("Location: /login");
 }
@@ -23,9 +23,35 @@ if(!isset($_SESSION['user'])) {
     include ROOT . "layout/navbar.php";
     ?>
 
-<main>
-
-</main>
+    <main>
+        <div class="container col-md-9">
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <div>
+                        <a href="#">
+                            Tema</a>
+                        <div class="mic-info">
+                            Od: <a href="#">Uzivatel</a> dne 2.10.2020
+                        </div>
+                    </div>
+                    <div class="comment-text my-2">
+                        Telo
+                    </div>
+                    <div class="action">
+                        <button type="button" class="btn btn-primary btn-xs" title="Edit">
+                            <i class="far fa-edit"></i>
+                        </button>
+                        <button type="button" class="btn btn-success btn-xs" title="Approved">
+                            <i class="fas fa-check"></i>
+                        </button>
+                        <button type="button" class="btn btn-danger btn-xs" title="Delete">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </main>
 
     <?php
     include ROOT . "layout/footer.php";
