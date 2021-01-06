21921270afb9371b31a6b8e23929af5242b2d00b
Ondřej Šrytr <ondrejsrytr@ondrejsrytr.cz>
2021-01-06 17:07:03 +0100

    Oprava přípon souborů

--- Kód/public_html/admin/articles-author/upload.php
+++ Kód/public_html/admin/articles-author/upload.php
@@ -12,13 +12,27 @@ $vysledek = $pdo->prepare($dotaz);
 $vysledek->execute(array($_POST["articleName"],$_SESSION["user"]->getId()));
 
 $last_id = $pdo->lastInsertId();
-$uploaddir = ROOT."/upload/";
-$uploadfile = $uploaddir . basename($last_id.".pdf");
+$uploaddir = ROOT."upload/";
+$extension = "";
+
+
+switch($_FILES['articleFile']['type']) {
+    case "application/pdf":
+        $extension = ".pdf";
+        break;
+    case "application/msword":
+    case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        $extension = ".docx";
+        break;
+}
+
+$uploadfile = $uploaddir . basename($last_id. $extension);
+
 echo $last_id;
 
 if (move_uploaded_file($_FILES['articleFile']['tmp_name'], $uploadfile)) {
-    echo "Upload byl dobrej"; //Ondřej je zlej ;-(
     ActivityLog::Log('Nahrán článek '.$last_id);
+    header("Location: index.php");
 } else {
     echo "Possible file upload attack!\n";
 }
